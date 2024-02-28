0d40d985083f7243415220d383f54e520bd536b5
unknown <lean.oliveira.sousa@gmail.com>
2024-02-28 07:12:16 -0300

    Criando entity Post com User aninhado

--- src/main/java/com/leandro/workshopmongo/resources/UserResource.java
+++ src/main/java/com/leandro/workshopmongo/resources/UserResource.java
@@ -3,7 +3,6 @@ package com.leandro.workshopmongo.resources;
 import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -15,7 +14,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
-
 import com.leandro.workshopmongo.domain.User;
 import com.leandro.workshopmongo.dto.UserDTO;
 import com.leandro.workshopmongo.services.UserService;
@@ -47,14 +45,14 @@ public class UserResource {
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
 		return ResponseEntity.created(uri).build();
 	}
-	
+
 	@DeleteMapping("/{id}")
 	public ResponseEntity<UserDTO> delete(@PathVariable String id) {
 		service.delete(id);
 		return ResponseEntity.noContent().build();
 	}
-	
-	@PutMapping(value="/{id}")
+
+	@PutMapping("/{id}")
 	public ResponseEntity<Void> update(@RequestBody UserDTO objDto, @PathVariable String id) {
 		User obj = service.fromDTO(objDto);
 		obj = service.insert(obj);
