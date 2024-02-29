e576e493f4232d193eb20599e69bcd0ad55d30a7
unknown <lean.oliveira.sousa@gmail.com>
2024-02-29 07:24:56 -0300

    Consulta simples com query methods

--- src/main/java/com/leandro/workshopmongo/resources/PostResource.java
+++ src/main/java/com/leandro/workshopmongo/resources/PostResource.java
@@ -1,13 +1,18 @@
 package com.leandro.workshopmongo.resources;
 
+import java.net.URLDecoder;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.leandro.workshopmongo.domain.Post;
+import com.leandro.workshopmongo.resources.util.URL;
 import com.leandro.workshopmongo.services.PostService;
 
 @RestController
@@ -17,6 +22,13 @@ public class PostResource {
 	@Autowired
 	private PostService service;
 
+	@GetMapping("/titlesearch")
+	public ResponseEntity<List<Post>> findByTitle(@RequestParam(value="text", defaultValue="") String text) {
+		text = URL.decodeParam(text);
+		List<Post> list = service.findByTitle(text);
+		return ResponseEntity.ok().body(list);
+	}
+	
 	@GetMapping("/{id}")
 	public ResponseEntity<Post> findById(@PathVariable String id) {
 		Post obj = service.findById(id);
